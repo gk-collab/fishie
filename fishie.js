06cb985e7c7844d62f0ff0fe7990f6c5406c3bad
George Hall <technikalmind@gmail.com>
2015-02-18 23:40:21 -0500

    Moveable boat
    The boat can now move left and right!

--- fishie.js
+++ fishie.js
@@ -1,22 +1,61 @@
 window.onload = function() {
 
 	var boat = {
-		id: "boat",
+		id: function() {
+			return document.getElementById("boat");
+		},
 		height: 40,
 		width: 80,
 		color: "black",
 		draw: function() {
-			var boatElement = document.getElementById(this.id);
-			if (boatElement.getContext) {
-			var ctx = boatElement.getContext('2d');
-			
-			ctx.fillRect(0,0,80,40);
+			if (this.id().getContext) {
+				var ctx = this.id().getContext('2d');
+				
+				ctx.fillRect(0,0,80,40);
 			}
-		  }
-		};
+		},
+		move: function(lr, increment) {
+			var getLeftProp = getComputedStyle(this.id()).getPropertyValue("left");
+
+			if(lr === "left") {
+				newPosition = parseFloat(getLeftProp) - increment;
+				this.id().style.left = newPosition;
+			} else if (lr === "right") {
+				newPosition = parseFloat(getLeftProp) + increment;
+				this.id().style.left = newPosition;
+			} else {
+				alert("An unexpected error has occurred.");
+			}
+			
+		}
+	};
 
 	boat.draw();
 	
+	
+	function leftArrowPressed() {
+	boat.move("left", 25);
+	}
+
+	function rightArrowPressed() {
+	boat.move("right", 25);
+	}
+	
+	document.onkeydown = function(evt) {
+		evt = evt || window.event; 
+		switch (evt.keyCode) {
+		case 37:
+		leftArrowPressed();
+		break;
+		case 39:
+		rightArrowPressed();
+		break;
+		}
+	};
+	
+	
+
+
 	
 	
 	
