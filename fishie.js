14fff439f5b823c645a9f8fdaa2604fa914626c1
George Hall <technikalmind@gmail.com>
2015-02-19 12:04:29 -0500

    Boat constraints

--- fishie.js
+++ fishie.js
@@ -1,44 +1,77 @@
 window.onload = function() {
 
+	var world = {
+	// this will be an object to define the world size later.
+	// the boat object will not move past world.left or world.right
+	
+	// world.size() = getViewportSize()
+	// world.left = 30px
+	// world.right = 30px less than viewport's right dimension
+		width: function() {
+			return w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+		},
+		
+		height: function() {
+			return h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+		},
+		
+		leftBorder: 35,
+		
+		rightBorder: function() {
+			return r = parseInt(this.width()) - 100;
+		}
+	};
+
 	var boat = {
 		id: function() {
 			return document.getElementById("boat");
 		},
+		// Define height, width, and color to be used when drawing
 		height: 40,
 		width: 80,
-		color: "black",
+		color: "#F4A460",
+		
 		draw: function() {
 			if (this.id().getContext) {
 				var ctx = this.id().getContext('2d');
 				
-				ctx.fillRect(0,0,80,40);
+				ctx.fillStyle = this.color;
+				ctx.fillRect(0,0,this.width,this.height);
 			}
 		},
-		move: function(lr, increment) {
+		
+		position: function() {
+			// Get the CSS 'left' property of #boat
 			var getLeftProp = getComputedStyle(this.id()).getPropertyValue("left");
-
-			if(lr === "left") {
-				newPosition = parseFloat(getLeftProp) - increment;
-				this.id().style.left = newPosition;
-			} else if (lr === "right") {
-				newPosition = parseFloat(getLeftProp) + increment;
-				this.id().style.left = newPosition;
-			} else {
-				alert("An unexpected error has occurred.");
-			}
+			return parseFloat(getLeftProp);
+		},
+		
+		move: function(lr, increment) {
+				
+				if(lr === "left" && this.position() > world.leftBorder) {
+					// If moving to the left, subtract from current value then update it
+					newPosition = this.position() - increment;
+					this.id().style.left = newPosition;
+				} else if (lr === "right" && this.position() <= world.rightBorder()) {
+					// If moving to the right, add to current value then update it
+					newPosition = this.position() + increment;
+					this.id().style.left = newPosition;
+				} else {
+					// I don't know what happened, boss!
+					// alert("An unexpected error has occurred.");
+				}
 			
 		}
 	};
 
 	boat.draw();
-	
-	
+		
 	function leftArrowPressed() {
-	boat.move("left", 25);
+		boat.move("left", 25);
 	}
 
 	function rightArrowPressed() {
-	boat.move("right", 25);
+		boat.move("right", 25);
 	}
 	
 	document.onkeydown = function(evt) {
@@ -52,30 +85,5 @@ window.onload = function() {
 		break;
 		}
 	};
-	
-	
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 };
